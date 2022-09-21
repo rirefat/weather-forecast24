7b9a97ce46b3d95540325e18e5b27345bb199d31
rirefat <rirefat.official@gmail.com>
2022-09-22 04:11:55 +0600

    set basic layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import './App.css';
+import hotBg from './assets/hot.jpg';
+import coldBg from './assets/cold.jpg';
+import logo from './assets/logo.png';
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='app' style={{backgroundImage: `url(${coldBg})`}}>
+      <div className="overlay">
+        <div className="container">
+          <div className="section section__inputs">
+            <input type="text" name='city' placeholder='Enter City...'/>
+            <button>°F</button>
+          </div>
+
+          <div className="section section__temperature">
+            <div className="icon">
+              <h3>London, GB</h3>
+              <img src={logo} alt="weather icon" className='dummy-img'/>
+              <h3>Cloudy</h3>
+            </div>
+
+            <div className="temperature">
+              <h1>34°C</h1>
+            </div>
+          </div>
+
+          {/* Bottom Description */}
+        </div>
+      </div>
     </div>
   );
 };
