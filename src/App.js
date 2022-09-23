c8bba25fa3d927ec33a7f603f04f4705caa3be1f
rirefat <rirefat.official@gmail.com>
2022-09-24 02:09:41 +0600

    done fetching problem

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import hotBg from './assets/hot.jpg';
 import coldBg from './assets/cold.jpg';
 import logo from './assets/logo.png';
 import Descriptions from './components/Descriptions/Descriptions';
+import { getFormattedWeatherData } from './WeatherService';
 
 const App = () => {
+  // useEffect(()=>{
+  //   const fetchWeatherData= async()=>{
+  //     const data = await getFormattedWeatherData("paris");
+  //   };
+  //   fetchWeatherData();
+  // },[])
+
+  const [info, setInfo]=useState([]);
+  useEffect(()=>{
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=dhaka&appid=2b67a6e7d72ce1a7a7c5eae9874cbe8b&units=metric')
+      .then(res=>res.json())
+      .then(data=>setInfo(data))    
+  },[]);
+
+  const {icon, description} = info.weather?info.weather[0]:"error";
+  const {temp, feels_like, temp_min, temp_max, pressure, humidity} = info.main ? info.main : "error";
+  const {country} = info.sys ? info.sys : "error";
+  const iconURL =`https://openweathermap.org/img/wn/${icon}@2x.png`;
+  const finalTemp = temp.toFixed(0);
+
+
   return (
     <div className='app' style={{backgroundImage: `url(${coldBg})`}}>
       <div className="overlay">
@@ -17,13 +39,13 @@ const App = () => {
 
           <div className="section section__temperature">
             <div className="icon">
-              <h3>London, GB</h3>
-              <img src="https://www.freeiconspng.com/thumbs/weather-icon-png/weather-icon-png-8.png" alt="weather icon" className='dummy-img'/>
-              <h3>Cloudy</h3>
+              <h3><span>{info.name}</span>, {country}</h3>
+              <img src={iconURL}/>
+              <h3>{description}</h3>
             </div>
 
             <div className="temperature">
-              <h1>34°C</h1>
+              <h1>{finalTemp}°C</h1>
             </div>
           </div>
 
