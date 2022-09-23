c0d2ce64c3090805343df73bd20a001af000c07a
rirefat <rirefat.official@gmail.com>
2022-09-23 20:26:50 +0600

    done single weather card

--- src/components/Descriptions/Descriptions.js
+++ src/components/Descriptions/Descriptions.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import './Descriptions.css';
+import { FiArrowDown } from "react-icons/fi";
 
 const Descriptions = () => {
     return (
         <div className='section section__description'>
             <div className="card">
                 <div className="desctiption__card-icon">
-                    
+                    <FiArrowDown></FiArrowDown>
+                    <small>min</small>
                 </div>
+                <h2>32°C</h2>
+            </div>
+            <div className="card">
+                <div className="desctiption__card-icon">
+                    <FiArrowDown></FiArrowDown>
+                    <small>min</small>
+                </div>
+                <h2>32°C</h2>
+            </div>
+            <div className="card">
+                <div className="desctiption__card-icon">
+                    <FiArrowDown></FiArrowDown>
+                    <small>min</small>
+                </div>
+                <h2>32°C</h2>
+            </div>
+            <div className="card">
+                <div className="desctiption__card-icon">
+                    <FiArrowDown></FiArrowDown>
+                    <small>min</small>
+                </div>
+                <h2>32°C</h2>
             </div>
         </div>
     );
